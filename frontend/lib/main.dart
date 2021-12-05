10f60e54976eeb9f21a2a9bda0b0c3afd6b63ab5
Zehua-Chen <zehua-chen@outlook.com>
2021-12-04 23:44:29 -0500

    [refactor] Create Replay Widget

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'player/player.dart';
-import 'schema/schema.dart' as schema;
+import 'replay.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,14 +27,6 @@ class MyStatefulWidget extends StatefulWidget {
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 1;
-  int _pageIndex = 0;
-  bool _playing = true;
-
-  final schema.Story _story = schema.Story([
-    schema.Page([schema.LivingCell(0, 0)]),
-    schema.Page([schema.LivingCell(-5, -5), schema.LivingCell(5, 5)]),
-    schema.Page([schema.LivingCell(-10, -10), schema.LivingCell(10, 10)])
-  ]);
 
   static Route<Object?> _dialogBuilder(
       BuildContext context, Object? arguments) {
@@ -46,19 +37,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
               applicationName: 'Conway Game of Life',
               applicationVersion: '1.0.0');
         });
-  }
-
-  void _nextPage() {
-    setState(() {
-      _pageIndex += 1;
-      _pageIndex = _pageIndex % _story.pages.length;
-    });
-  }
-
-  void _playingToggle() {
-    setState(() {
-      _playing = !_playing;
-    });
   }
 
   String _title() {
@@ -77,12 +55,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       case 0:
         return const Placeholder();
       case 1:
-        return ConwayPlayer(
-            story: _story,
-            page: _pageIndex,
-            playing: _playing,
-            onNextPage: _nextPage,
-            onPlayingToggle: _playingToggle);
+        return const Replay();
       default:
         return const Placeholder();
     }
---
+++ frontend/lib/replay.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:conway/player/player.dart';
+import 'package:conway/schema/schema.dart' as schema;
+
+class Replay extends StatefulWidget {
+  const Replay({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ReplayState();
+}
+
+class _ReplayState extends State<Replay> {
+  int _pageIndex = 0;
+  bool _playing = true;
+
+  final schema.Story _story = schema.Story([
+    schema.Page([schema.LivingCell(0, 0)]),
+    schema.Page([schema.LivingCell(-5, -5), schema.LivingCell(5, 5)]),
+    schema.Page([schema.LivingCell(-10, -10), schema.LivingCell(10, 10)])
+  ]);
+
+  void _nextPage() {
+    setState(() {
+      _pageIndex += 1;
+      _pageIndex = _pageIndex % _story.pages.length;
+    });
+  }
+
+  void _playingToggle() {
+    setState(() {
+      _playing = !_playing;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ConwayPlayer(
+        story: _story,
+        page: _pageIndex,
+        playing: _playing,
+        onNextPage: _nextPage,
+        onPlayingToggle: _playingToggle);
+  }
+}
