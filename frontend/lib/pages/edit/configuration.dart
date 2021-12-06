c329ab8246bda04b62c5880f69e1e231674f1359
Zehua-Chen <zehua-chen@outlook.com>
2021-12-05 20:13:19 -0500

    [feat] Add Paddings to Configuration

--- frontend/lib/pages/edit/configuration.dart
+++ frontend/lib/pages/edit/configuration.dart
@@ -27,14 +27,20 @@ class _ConfigurationState extends State<Configuration> {
         child: Form(
             key: _form,
             child: Column(children: <Widget>[
-              NumberFormField(
-                  value: _width,
-                  label: const Text('Width'),
-                  onValueChanged: (value) => setState(() => _width = value)),
-              NumberFormField(
-                  value: _height,
-                  label: const Text('Height'),
-                  onValueChanged: (value) => setState(() => _height = value)),
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: NumberFormField(
+                      value: _width,
+                      label: const Text('Width'),
+                      onValueChanged: (value) =>
+                          setState(() => _width = value))),
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: NumberFormField(
+                      value: _height,
+                      label: const Text('Height'),
+                      onValueChanged: (value) =>
+                          setState(() => _height = value))),
               Padding(
                   padding: const EdgeInsets.all(16),
                   child: ElevatedButton.icon(
