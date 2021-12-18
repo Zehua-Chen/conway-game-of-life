3759276554c0611d1a3ae4907319addbca64585c
Zehua-Chen <zehua-chen@outlook.com>
2021-12-18 16:04:41 -0500

    [feat] Add HUnit and Test Framework

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit (testCase)
+import Test.HUnit
+
+tests =
+  [ testGroup
+      "simulate"
+      [ testCase
+          ""
+          ( do
+              assertBool "" True
+          )
+      ]
+  ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
