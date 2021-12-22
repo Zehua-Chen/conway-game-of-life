223511f7473f308000958a3f6bd71bbf38d37980
Zehua-Chen <zehua-chen@outlook.com>
2021-12-22 15:10:44 -0500

    [feat] Add Benchmark

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,19 @@ bigWorld = World.fromList (replicate 101 (replicate 101 True))
 main :: IO ()
 main =
   defaultMain
-    [ bench "async" async,
-      bench "sync" sync
+    [ bench "sync" sync,
+      bgroup
+        "async"
+        [ bench "w: 10, h: 10, chunk: 10" (async 10 10 10),
+          bench "w: 20, h: 20, chunk: 10" (async 20 20 10),
+          bench "w: 40, h: 40, chunk: 10" (async 40 40 10),
+          bench "w: 80, h: 80, chunk: 10" (async 80 80 10),
+          bench "w: 40, h: 40, chunk: 10" (async 40 40 10),
+          bench "w: 40, h: 40, chunk: 20" (async 40 40 20),
+          bench "w: 40, h: 40, chunk: 40" (async 40 40 40),
+          bench "w: 40, h: 40, chunk: 80" (async 40 40 80)
+        ]
     ]
   where
-    async = nf (simulateAsync 40 40 20) bigWorld
     sync = nf simulateSync bigWorld
+    async w h c = nf (simulateAsync w h c) bigWorld
